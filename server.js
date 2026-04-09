871059889afb3eaae6ded0af6b549e3c2872020d
Thanga-Akilan-cdw <88623636+TyroAurum@users.noreply.github.com>
2026-04-09 11:33:40 +0530

    deploy commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const logger = require("./logger");
 
 const PORT = process.env.PORT || 3000;
+const helo = "Hello, World!";
 
 app.listen(PORT, () => {
   logger.info(`Order service running at http://localhost:${PORT}`);
